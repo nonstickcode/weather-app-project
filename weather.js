b0e5cb158c52b6b9e62fb1fa4821db45a0662253
Cody <66153585+nonstickcode@users.noreply.github.com>
2023-12-13 14:36:15 -0700

    s

--- weather.js
+++ weather.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export function getWeather(lat, lon, timezone) {
   return axios
     .get(
-      "http://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime",
+      "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime",
       {
         params: {
           latitude: lat,
